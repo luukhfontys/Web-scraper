fe4e62c65dbbf26d4119fe6046e9fd397dfb43c2
luukhfontys <l.hooymans@student.fontys.nl>
2024-01-14 10:45:50 +0100

    ver. 1

---
+++ functions.py
@@ -0,0 +1,174 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import streamlit as st
+from io import BytesIO
+from pyxlsb import open_workbook as open_xlsb
+
+def get_car_models():
+    """"Haalt alle modellen autos van gaspedaal.nl af en returned dictionary met {'Merk': ['Modellen', ...]}"""
+    url = 'https://www.gaspedaal.nl/blog/autoblog/auto-abc/22-automerken'
+    html = requests.get(url)
+
+    s = BeautifulSoup(html.content, 'html.parser')
+
+    # Vind alle auto merken met model.
+    results = s.find_all('li')
+
+    text_list = []
+
+    for li in results:
+        text_content = li.text.strip()  # Use strip() to remove leading and trailing whitespaces
+        text_list.append(text_content)
+
+    filtered_text_list = [item for item in text_list if "|" in item]
+    result_dict = {item.split(' | ')[0]: item.split(' | ')[1].split(', ') for item in filtered_text_list}
+    result_dict = {item.split(' | ')[0].replace('.', '').replace(' ', '-'): [model.replace('.', '').replace(' ', '-') for model in item.split(' | ')[1].split(', ')] for item in filtered_text_list}
+    
+    return result_dict
+
+def construct_search_query(make, model=None, bmin=None, bmax=None, importEU=False):
+    query_params = {'bmin': bmin, 'bmax': bmax}
+
+    if model:
+        # If model is specified, include it in the query
+        search_query = f"{make}/{model}"
+    else:
+        # If model is not specified, only include the make in the query
+        search_query = make
+
+    # Append the query parameters
+    query_string = "&".join([f"{key}={value}" for key, value in query_params.items() if value])
+
+    # Final search query
+    final_query = f"{search_query}?{query_string}" if query_string else search_query
+    if importEU:
+        final_query = 'https://www.gaspedaal.nl/importautos/' + final_query
+    else:
+        final_query = 'https://www.gaspedaal.nl/' + final_query
+
+    if '?' in final_query:
+        final_query += '&srt=df-a'
+    else:
+        final_query += '?srt=df-a'
+        
+    return final_query
+
+def extract_bouwjaar_km(info_string):
+    
+    bouwjaar_match = re.search(r'Bouwjaar:\s*(\d+)', info_string)
+    km_stand_match = re.search(r'Km.stand:\s*([\d.]+)\s*km', info_string)
+
+    bouwjaar_value = bouwjaar_match.group(1) if bouwjaar_match else None
+    km_stand_value = km_stand_match.group(1).replace(".", "") if km_stand_match else None
+    
+    return bouwjaar_value, km_stand_value
+
+def extract_properties_of_car(car):
+    
+    volledige_naam = car.find('h4').text.strip()
+    properties = car.find_all('p')
+    website = 'https://www.gaspedaal.nl' + car.get('href')
+    
+    prop_strip = []
+
+    for i in range(len(properties)):
+        prop_strip.append(properties[i].text.strip())
+
+    bouwjaar, km_stand = extract_bouwjaar_km(prop_strip[1])
+    prijs = prop_strip[0].replace(".", "")
+    
+    locatie = prop_strip[-2]
+    
+    return [volledige_naam, prijs, bouwjaar, km_stand, locatie, website]
+
+def extract_page_info(page):
+    return page.find_all(attrs={"data-testid": "occasion-item"})
+
+def get_elements(merken: list, modellen: list, importEU: bool):
+    elements_list = []
+    merkenindex = []
+    modellenindex = []
+    for merk, model in zip(merken, modellen):
+        url = construct_search_query(merk, model=model, bmin=None, bmax=None)
+        html = requests.get(url)
+        page = BeautifulSoup(html.content, 'html.parser')
+        elements = extract_page_info(page)
+        
+        i = 2
+        while len(elements) > 0:
+            elements_list.append(elements)
+            urlpage = url + '&page=' + str(i)
+            html = requests.get(urlpage)
+            page = BeautifulSoup(html.content, 'html.parser')
+            elements = extract_page_info(page)
+            i += 1
+            merkenindex.append(merk)
+            modellenindex.append(model)
+    
+    if importEU:
+        for merk, model in zip(merken, modellen):
+            url = construct_search_query(merk, model=model, importEU=importEU)
+            html = requests.get(url)
+            page = BeautifulSoup(html.content, 'html.parser')
+            elements = extract_page_info(page)
+        
+        i = 2
+        while len(elements) > 0:
+            elements_list.append(elements)
+            urlpage = url + '&page=' + str(i)
+            html = requests.get(urlpage)
+            page = BeautifulSoup(html.content, 'html.parser')
+            elements = extract_page_info(page)
+            i += 1
+            merkenindex.append(merk)
+            modellenindex.append(model)
+            
+    total_elements = sum(len(inner_list) for inner_list in elements_list)
+    return elements_list, merkenindex, modellenindex, total_elements
+  
+def scrape_data_df(element_list: list, merkenindex: list, modellenindex: list, progress_bar=None) -> pd.DataFrame:
+    total_elements = sum(len(inner_list) for inner_list in element_list)
+    property_list = []
+    counter = 0
+    i = 0
+    for elements in element_list:
+
+        url = construct_search_query(merkenindex[i], model=modellenindex[i], bmin=None, bmax=None)
+        for element in elements:
+            property_temp = extract_properties_of_car(element)
+            for j in range(1, 4):
+                property_temp[j] = int(property_temp[j])
+            property_list.append([f'{merkenindex[i]} {modellenindex[i]}'] + property_temp)
+            counter += 1
+            
+        i += 1
+        progresscount = counter/total_elements
+        if progress_bar is not None:
+            progress_bar.progress(progresscount)
+            if counter % 100 == 0:
+                st.rerun()
+
+    columns = ['Naam auto', 'Volledige naam', 'Prijs (euro)', 'Bouwjaar', 'Kilometer stand', 'Locatie', 'Website']
+    df = pd.DataFrame(property_list, columns = columns)
+
+    return df
+# Testing:
+
+
+x=1
+
+merken = ['tesla', 'volkswagen']
+modellen = ['model-3', 'ID3']
+
+# test = get_elements(merken, modellen)
+
+# x= 1
+# url = construct_search_query(merk, model=model, bmin=None, bmax=None)
+
+# df = scrape_data_df(url)
+
+# df.to_excel(f'Resultaat_{merk}_{model}.xlsx')
+
+# x=1
---
+++ main.py
@@ -0,0 +1,87 @@
+import streamlit as st
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+from functions import *
+import xlsxwriter
+import io
+
+st.set_page_config(
+    page_title='Gaspedaal webscraper',         #Titel in browser
+    layout="wide",                          #Type pagina, deze is breed zodat hij het hele scherm vult
+    page_icon="🚗",                         #Icoontje van pagina
+    initial_sidebar_state="expanded",       #Zorgen dat het menu gelijk open staat
+)
+
+if 'page' not in st.session_state:
+    st.session_state['page'] = 'Keuze'
+    st.session_state.selected_brand = None
+    st.session_state.Geselecteerde_autos = []
+    st.session_state.elements_list = []
+    
+def Keuze():
+    st.title('Gaspedaal.nl webscraper')
+    opties = get_car_models()
+    selected_brand = st.selectbox('Selecteer merk:', list(opties.keys()))
+    st.session_state.selected_brand = selected_brand
+    if st.session_state.selected_brand:
+        # Get the models for the selected brand
+        selected_models = opties[st.session_state.selected_brand]
+
+        # Dropdown for selecting a model
+        selected_model = st.selectbox('Selecteer model:', selected_models)
+        if selected_model:
+            # Button to add the selected car to the list
+            if st.button('Toevoegen aan scrape lijst'):
+                # Initialize the list if not already created
+                if 'Geselecteerde_autos' not in st.session_state:
+                    st.session_state.Geselecteerde_autos = []
+                    
+                # Add the selected car to the list
+                if {'brand': selected_brand, 'model': selected_model} not in st.session_state.Geselecteerde_autos:
+                    st.session_state.Geselecteerde_autos.append({'brand': selected_brand, 'model': selected_model})
+    if 'Geselecteerde_autos' in st.session_state and st.session_state.Geselecteerde_autos:
+        df_selected_cars = pd.DataFrame(st.session_state.Geselecteerde_autos)
+        st.dataframe(df_selected_cars)
+        if st.button('Clear selectie'):
+            st.session_state.Geselecteerde_autos = []
+            st.rerun()
+        
+        index = list(df_selected_cars.index.values)
+        brand = list(df_selected_cars.brand.values)
+        model = list(df_selected_cars.model.values)
+
+        # Create a list of strings by concatenating index, brand, and model
+        merged_list = [f"{idx} {brand} {model}" for idx, brand, model in zip(index, brand, model)]
+
+        remove_select = st.selectbox('Verwijder selectie:', merged_list)
+        if st.button('Verwijder'):
+            lijst_temp = st.session_state.Geselecteerde_autos
+            lijst_temp.pop(int(remove_select[0]))
+            st.rerun()
+            
+        importEU = st.checkbox('EU import auto\'s ook meenemen?')
+        if st.button('Start scrape'):
+            merken = [car["brand"] for car in st.session_state.Geselecteerde_autos]
+            modellen = [car["model"] for car in st.session_state.Geselecteerde_autos]
+            progress_bar = st.progress(0)
+            elements, merkenindex, modellenindex, total_elements = get_elements(merken, modellen, importEU)
+            st.success(f'{total_elements} matches found!')
+            df = scrape_data_df(elements, merkenindex, modellenindex, progress_bar)
+            st.dataframe(df)
+            buffer = io.BytesIO()
+            df.to_excel(buffer, sheet_name='Sheet1', index=False, engine='xlsxwriter')
+            buffer.seek(0)
+            download_button = st.download_button(
+                label="Download data als Excel",
+                data=buffer,
+                file_name='Gaspedaal_scraped_data.xlsx',
+                mime='application/vnd.ms-excel'
+            )
+
+            # Display the download button
+            if download_button:
+                st.success("Downloaded successfully!")
+            
+if st.session_state['page'] == 'Keuze':
+    Keuze()
